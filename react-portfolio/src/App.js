16b5850754993e0f794e145747ace345313a7985
Cliff Ndukuya <84091410+Cliffndukuya@users.noreply.github.com>
2023-03-26 16:04:38 +0200

    update

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+ 
+
 function App() {
+ const projects = [
+    { name: 'Project 1', description: 'This is project 1.' },
+    { name: 'Project 2', description: 'This is project 2.' },
+    { name: 'Project 3', description: 'This is project 3.' },
+  ];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class='main'>
+
+
+      <div class='about'>
+        <h1>About Me </h1>
+      </div>
+
+      <div class='proj'>
+      <h2>IT Portfolio</h2>
+      <ul>
+        {projects.map((project, index) => (
+          <li key={index}>
+            <h2>{project.name}</h2>
+            <p>{project.description}</p>
+          </li>
+        ))}
+      </ul>
+      </div>
+
     </div>
   );
 }
+
 
 export default App;
